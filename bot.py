e8d40c4dc6fe3e22a538542ba7b829a7e945fd48
Koji Shiraishi <shiraco@gmail.com>
2016-10-14 15:16:58 +0900

    apply messaging api

--- bot.py
+++ bot.py
@@ -14,17 +14,16 @@ handler.setLevel(DEBUG)
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
-ENDPOINT_URI = 'https://trialbot-api.line.me/v1/events'
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 DOCOMO_API_KEY = os.environ.get('DOCOMO_API_KEY', '')
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', '')
 
 
 class CallbackResource(object):
     # line
     header = {
         'Content-Type': 'application/json; charset=UTF-8',
-        'X-Line-ChannelID': os.environ['LINE_CHANNEL_ID'],
-        'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
-        'X-Line-Trusted-User-With-ACL': os.environ['LINE_CHANNEL_MID'],
+        'Authorization': 'Bearer {}'.format(LINE_CHANNEL_ACCESS_TOKEN)
     }
 
     # docomo
@@ -41,38 +40,41 @@ class CallbackResource(object):
         receive_params = json.loads(body.decode('utf-8'))
         logger.debug('receive_params: {}'.format(receive_params))
 
-        for msg in receive_params['result']:
+        for event in receive_params['events']:
 
-            logger.debug('msg: {}'.format(msg))
+            logger.debug('event: {}'.format(event))
 
-            try:
-                docomo_res = self.docomo_client.send(
-                    utt=msg['content']['text'], apiname='Dialogue')
+            if event['type'] == 'message':
+                try:
+                    user_utt = event['message']['text']
+                    docomo_res = self.docomo_client.send(
+                        utt=user_utt, apiname='Dialogue')
 
-            except Exception:
-                raise falcon.HTTPError(falcon.HTTP_503,
-                                       'Docomo API Error. ',
-                                       'Could not invoke docomo api.')
+                except Exception:
+                    raise falcon.HTTPError(falcon.HTTP_503,
+                                           'Docomo API Error. ',
+                                           'Could not invoke docomo api.')
 
-            logger.debug('docomo_res: {}'.format(docomo_res))
+                logger.debug('docomo_res: {}'.format(docomo_res))
+                sys_utt = docomo_res['utt']
 
-            send_content = {
-                'to': [msg['content']['from']],
-                'toChannel': 1383378250,  # Fixed value
-                'eventType': '138311608800106203',  # Fixed value
-                'content': {
-                    'contentType': 1,
-                    'toType': 1,
-                    'text': docomo_res['utt'],
-                },
-            }
-            send_content = json.dumps(send_content)
-            logger.debug('send_content: {}'.format(send_content))
+                send_content = {
+                    'replyToken': event['replyToken'],
+                    'messages': [
+                        {
+                            'type': 'text',
+                            'text': sys_utt
+                        }
 
-            res = requests.post(ENDPOINT_URI, data=send_content, headers=self.header)
-            logger.debug('res: {} {}'.format(res.status_code, res.reason))
+                    ]
+                }
+                send_content = json.dumps(send_content)
+                logger.debug('send_content: {}'.format(send_content))
 
-            resp.body = json.dumps('OK')
+                res = requests.post(ENDPOINT_URI, data=send_content, headers=self.header)
+                logger.debug('res: {} {}'.format(res.status_code, res.reason))
+
+                resp.body = json.dumps('OK')
 
 
 api = falcon.API()
